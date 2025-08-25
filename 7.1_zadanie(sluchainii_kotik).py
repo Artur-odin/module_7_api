c046df2e9f2c30db23cd2936703c57be911d3f12
Artur-odin <galeevartur1986@gmail.com>
2025-08-25 14:03:50 +0300

    добавил кнопку в отдельном фрейме.

--- 7.1_zadanie(sluchainii_kotik).py
+++ 7.1_zadanie(sluchainii_kotik).py
@@ -4,7 +4,8 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-Allowed_tags = ["sleep", "jump", "fight", "black", "white", "siamese", "cute"]
+# Список доступных тегов
+ALLOWED_TAGS = ['sleep', 'jump', 'smile', 'fight', 'black', 'white', 'red', 'siamese', 'bengal']
 
 def load_image(url):
     try:
@@ -14,25 +15,29 @@ def load_image(url):
         img = Image.open(image_data)
         img.thumbnail((600, 480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
-    except Exception as e:
-        print(f"Произошла ошибка: {e}")
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при загрузке изображения: {e}")
         return None
 
 def open_new_window():
     tag = tag_combobox.get()
-    url_tag = f"https://cataas.com/cat/{tag}" if tag else url
-    img = load_image(url_tag)
+    url_with_tag = f"https://cataas.com/cat/{tag}" if tag else 'https://cataas.com/cat'
+    img = load_image(url_with_tag)
     if img:
         new_window = Toplevel()
-        new_window.title("Картинки с котиками")
+        new_window.title("Cat Image")
         new_window.geometry("600x480")
         label = Label(new_window, image=img)
-        label.pack()
         label.image = img
-
+        label.pack()
+        
 window = Tk()
-window.title("Cats")
+window.title("Cats!")
 window.geometry("600x520")
+frame = Frame(window)
+frame.pack()
+button_random_cat = Button(frame, text="Cлучайный котик", command=(open_new_window))
+button_random_cat.pack()
 
 
 menu_bar = Menu(window)
@@ -44,15 +49,11 @@ file_menu.add_command(label="Загрузить фото", command=open_new_wind
 file_menu.add_separator()
 file_menu.add_command(label="Выход", command=window.destroy)
 
-url = "https://cataas.com/cat"
-
-tag_label = Label(text="Выбери тег")
+# Метка "Выбери тег"
+tag_label = Label(window, text="Выбери тег")
 tag_label.pack()
 
-tag_combobox = ttk.Combobox(values=Allowed_tags)
+tag_combobox = ttk.Combobox(window, values=ALLOWED_TAGS)
 tag_combobox.pack()
-
-load_button = Button(text="Загрузить по тегу", command=open_new_window)
-load_button.pack()
 
 window.mainloop()
