32bf930e9f5a0cfb3b9b4ee7a96ce01a1a1d76c7
Artur-odin <galeevartur1986@gmail.com>
2025-08-25 16:03:26 +0300

    по тегу открывает отдельное окно, по рандому вкладку с котиком

--- 7.1_zadanie(sluchainii_kotik).py
+++ 7.1_zadanie(sluchainii_kotik).py
@@ -6,6 +6,8 @@ from io import BytesIO
 
 # Список доступных тегов
 ALLOWED_TAGS = ['sleep', 'jump', 'smile', 'fight', 'black', 'white', 'red', 'siamese', 'bengal']
+l = []
+
 
 def load_image(url):
     try:
@@ -19,10 +21,32 @@ def load_image(url):
         print(f"Ошибка при загрузке изображения: {e}")
         return None
 
-def open_new_window():
+def teg_cate_url():
     tag = tag_combobox.get()
-    url_with_tag = f"https://cataas.com/cat/{tag}" if tag else 'https://cataas.com/cat'
-    img = load_image(url_with_tag)
+    url = f"https://cataas.com/cat/{tag}" if tag else 'https://cataas.com/cat'
+    open_new_window(url)
+
+def random_cat_url():
+    #open_new_window("https://cataas.com/cat")
+    tab = Frame(notebook)
+    notebook.add(tab, text="Котик")
+
+    close_button = Button(tab, text="✕", command=lambda: close_tab(tab))
+    close_button.pack(anchor="ne")
+
+    img = load_image("https://cataas.com/cat")
+    if img:
+        label = Label(tab, image=img)
+        label.image = img
+        label.pack()
+    notebook.select(tab)
+    def close_tab(tab):
+        notebook.forget(tab)
+
+    
+
+def open_new_window(url):
+    img = load_image(url)
     if img:
         new_window = Toplevel()
         new_window.title("Cat Image")
@@ -34,26 +58,34 @@ def open_new_window():
 window = Tk()
 window.title("Cats!")
 window.geometry("600x520")
-frame = Frame(window)
-frame.pack()
-button_random_cat = Button(frame, text="Cлучайный котик", command=(open_new_window))
-button_random_cat.pack()
 
+frame_1 = Frame(window)
+frame_1.pack()
+frame_2 = Frame(window)
+frame_2.pack()
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
 
 file_menu = Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="Файл", menu=file_menu)
-file_menu.add_command(label="Загрузить фото", command=open_new_window)
+file_menu.add_command(label="Загрузить фото", command=teg_cate_url)
 file_menu.add_separator()
 file_menu.add_command(label="Выход", command=window.destroy)
 
-# Метка "Выбери тег"
-tag_label = Label(window, text="Выбери тег")
-tag_label.pack()
+tag_label = Label(frame_1, text="Выбери тег")
+tag_label.grid(row=0, column=0)
 
-tag_combobox = ttk.Combobox(window, values=ALLOWED_TAGS)
-tag_combobox.pack()
+tag_combobox = ttk.Combobox(frame_1, values=ALLOWED_TAGS)
+tag_combobox.grid(row=1, column=0)
+
+button_teg_cat = Button(frame_1, text="Загрузить фото", command=teg_cate_url)
+button_teg_cat.grid(row=1, column=1)
+
+button_random_cat = Button(frame_1, text="Cлучайный котик", command=random_cat_url)
+button_random_cat.grid(row=1, column=2)
+
+notebook = ttk.Notebook(window)
+notebook.pack(fill=BOTH, expand=True)
 
 window.mainloop()
