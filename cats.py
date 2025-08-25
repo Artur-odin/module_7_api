b5e636e567bee62fa5129c62c722d0427d1d9d2d
Artur-odin <galeevartur1986@gmail.com>
2025-08-25 12:31:12 +0300

    добавлено получение изображения по тегу и без тега

--- cats.py
+++ cats.py
@@ -16,7 +16,9 @@ def load_image(url):
         return None
 
 def open_new_window():
-    img = load_image(url)
+    tag = tag_entry.get()
+    url_tag = f"https://cataas.com/cat/{tag}" if tag else url
+    img = load_image(url_tag)
     if img:
         new_window = Toplevel()
         new_window.title("Картинки с котиками")
@@ -32,8 +34,12 @@ window = Tk()
 window.title("Cats")
 window.geometry("600x520")
 
-# update_button = Button(text="Обновить", command=set_image)
-# update_button.pack()
+tag_entry = Entry()
+tag_entry.pack()
+
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
+
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
